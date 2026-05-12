dd879125527926540f887afbef7a8483485e6a2d
yahya-dev07 <mohammedyehaya913@gmail.com>
2026-05-12 15:40:35 +0530

    Fixed Render database connection

--- src/config/db.js
+++ src/config/db.js
@@ -1,11 +1,18 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-    user: "postgres",
-    host: "localhost",
-    database: "smart_url_shortener",
-    password: "yahya123",
-    port: 5432,
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
+
+pool.connect()
+    .then(() => {
+        console.log("PostgreSQL Connected Successfully");
+    })
+    .catch((err) => {
+        console.log("Database Connection Error:", err);
+    });
 
 module.exports = pool;
