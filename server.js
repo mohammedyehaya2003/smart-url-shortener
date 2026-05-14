1738a592603245b7189316e0682d9d2a1d8927a9
yahya-dev07 <mohammedyehaya913@gmail.com>
2026-05-14 16:59:18 +0530

    Added CORS support

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
 const pool = require('./src/config/db');
 
 //Allows server to read JSON data from client//
 app.use(express.json());
+
+app.use(cors());
 
 // import routes
 const urlRoutes = require('./src/routes/urlRoutes');
